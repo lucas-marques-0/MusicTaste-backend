463d6e6cc185937513fbfd4119c40f3f864a1112
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-11-28 08:16:44 -0300

    corrigindo bugs

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ server.post('/usuarios', async (request, reply) => {
     return reply.status(201).send()
 })
 
-server.post('/usuarios', async (request, reply) => {
+server.post('/login', async (request, reply) => {
   const { userID, password } = request.body
   const userInfo = await database.buscarUsuarioID(userID)
   const loginPassword = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex)
