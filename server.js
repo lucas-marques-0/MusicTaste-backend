a6589cefa28669fdd6d51f8525efe6c4740b849b
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-04 10:36:28 -0300

    corrigindo rota post com autenticação

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
 import { DatabasePostgres } from './database-postgres.js';
@@ -7,14 +7,7 @@ const app = express();
 const port = process.env.PORT || 3333;
 const database = new DatabasePostgres();
 
-//app.use(cors({ origin: '*' }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next()
-});
+app.use(cors({ origin: '*' }));
 
 const authenticatedRouteOptions = (req, res, next) => {
   const token = req.headers.authorization?.replace(/^Bearer /, '');
