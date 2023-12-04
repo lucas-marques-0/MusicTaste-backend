c15624c10882aff9a633a7b6f449088991e09c88
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-04 09:11:55 -0300

    corrigindo rota post com autenticação

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const database = new DatabasePostgres();
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'https://musictasteshare.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization'); // Adicionado 'Authorization'
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
