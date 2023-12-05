fe1811507c5764f456c5f1edf074c0ff7c499cfe
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-05 10:05:18 -0300

    corrigindo erro de cors

--- server.js
+++ server.js
@@ -7,21 +7,6 @@ const app = express();
 const port = process.env.PORT || 3333;
 const database = new DatabasePostgres();
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
 app.use(cors({
   origin: 'https://musictasteshare.vercel.app',
 }));
