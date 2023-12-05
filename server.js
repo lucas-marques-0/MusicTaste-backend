32e1bcbeb8debc7404773fd2077b7d883e6e26f6
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-05 13:03:32 -0300

    corrigindo erro cors na  rota

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3333;
 const database = new DatabasePostgres();
 
 app.use(cors({
-  origin: 'https://musictasteshare.vercel.app',
+  origin: 'https://musictaste-backend.onrender.com',
 }))
 
 const authenticateToken = (req, res, next) => {
