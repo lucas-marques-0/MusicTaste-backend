b0d40c545b7ce40e1a7ac56c71f4cb00bbc6e661
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-05 09:53:57 -0300

    corrigindo erro de cors

--- server.js
+++ server.js
@@ -24,19 +24,6 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
-
-
-
-
-
-
-
 app.use(cors({
   origin: 'https://musictasteshare.vercel.app',
 }));
