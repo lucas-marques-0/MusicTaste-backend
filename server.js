90536183cefdbfa4882a9e421ad65630f77b7562
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-06 09:09:15 -0300

    corrigindo erro cors na  rota

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
   
-  if (token == null || undefined) {
+  if (!token) {
     return res.status(401).json({ message: 'Achou o token.' })
   };
 
