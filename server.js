0f28a6dcfc85ad2960350355773a251e3e262db5
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-06 10:25:25 -0300

    corrigindo erro cors na  rota

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 }))
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.headers['Authorization']
   const token = authHeader && authHeader.split(' ')[1]
   
   if (!token) {
