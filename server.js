d010de39b7f10430d2123df2d4882b462977a857
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-06 11:31:24 -0300

    corrigindo erro cors na  rota

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/usuarios', async (req, res) => {
     const userPassword = userInfo[0].password;
     if (userPassword === password) {
       const token = jwt.sign({ id: userInfo.id, email: userInfo.email }, 'segredo-do-jwt', {
-        expiresIn: 60,
+        expiresIn: '1d',
       });
       const userObject = { ...userInfo[0], password: undefined };
       return res.status(201).json({ token, user: userObject });
