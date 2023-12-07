8e88b84d02fc01ba96789be5e285838d8fca4c63
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-07 09:29:58 -0300

    corrigindo envio de token por header - mudança pra post

--- server.js
+++ server.js
@@ -14,14 +14,12 @@ app.use(cors({
 app.use(express.json());
 
 const authenticateToken = (req, res, next) => {
-  const token = req.body.token
-  
+  const { token } = req.body;
   if (!token) return res.status(401).json({ message: 'Não achou o token.' })
 
-  const user = verifyToken(token);
-  if (!user) return res.status(404).json({ message: 'Unauthorized: invalid token.' })
+  const verifyToken = jwt.verify(token, 'segredo-do-jwt');
+  if (!verifyToken) return res.status(404).json({ message: 'Token inválido.' })
 
-  req.user = user;
   next();
 }
 
@@ -44,7 +42,7 @@ app.post('/usuarios', async (req, res) => {
     const userPassword = userInfo[0].password;
     if (userPassword === password) {
       const token = jwt.sign({ id: userInfo.id, email: userInfo.email }, 'segredo-do-jwt', {
-        expiresIn: '1d',
+        expiresIn: 60,
       });
       const userObject = { ...userInfo[0], password: undefined };
       return res.status(201).json({ token, user: userObject });
@@ -65,13 +63,7 @@ app.get('/usuarios', async (req, res) => {
   return res.json(userObjects);
 });
 
-app.post('/usuarios/:id', async (req, res) => {
-  const { token } = req.body;
-  if (!token) return res.status(401).json({ message: 'Não achou o token.' })
-
-  const verifyToken = jwt.verify(token, 'segredo-do-jwt');
-  if (!verifyToken) return res.status(404).json({ message: 'Token inválido.' })
-
+app.post('/usuarios/:id', authenticateToken, async (req, res) => {
   const userID = req.params.id;
   const userInfo = await database.buscarUsuarioID(userID);
   return res.status(201).json({ userInfo, token: token, tokenValido: verifyToken });
