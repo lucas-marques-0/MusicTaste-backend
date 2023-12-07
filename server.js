8b6022cf56aa93ee630816966fbaf6847d8ddc1b
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-07 00:17:04 -0300

    corrigindo envio de token por header - mudança pra post

--- server.js
+++ server.js
@@ -71,11 +71,10 @@ app.post('/usuarios/:id', async (req, res) => {
 
   const verifyToken = jwt.verify(token, 'segredo-do-jwt');
   if (!verifyToken) return res.status(404).json({ message: 'Token inválido.' })
-  const verifyTokenTeste = jwt.verify(123, 'segredo-do-jwt');
 
   const userID = req.params.id;
   const userInfo = await database.buscarUsuarioID(userID);
-  return res.status(201).json({ userInfo, token: token, tokenValido: verifyToken, teste: verifyTokenTeste });
+  return res.status(201).json({ userInfo, token: token, tokenValido: verifyToken });
 });
 
 app.put('/usuarios/:id', async (req, res) => {
