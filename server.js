95c94b955dc9ad51f33172315d736bcdb724d112
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-11 08:45:34 -0300

    Removendo senha do retorno da req

--- server.js
+++ server.js
@@ -64,8 +64,7 @@ app.get('/usuarios', async (req, res) => {
 app.post('/usuarios/:id', authenticateToken, async (req, res) => {
   const userID = req.params.id;
   const userInfos = await database.buscarUsuarioID(userID);
-  const userObject = { ...userInfos[0], password: undefined };
-  return res.status(201).json({ userObject });
+  return res.status(201).json({ userInfos });
 });
 
 app.put('/usuarios/:id', authenticateToken, async (req, res) => {
